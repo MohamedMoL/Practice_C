22e136b3c1db94841e2eecfe8f6ad2627f79ddb2
MohamedML <mohamed.moussati.lamhamdi@gmail.com>
2021-09-20 17:51:39 +0100

    More exercises and comments

--- hello-world.c
+++ hello-world.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//----------------------------------Practice of variables and operations---------------------------------------
 /*int main(){
     int x, y, z;
     printf("Da el valor de x: ");
@@ -26,12 +27,68 @@
     printf("El volumen del cilindro es: %f\n", volumen);
     return 0;
 }*/
-int main(){
+/*int main(){
     int fahr;
     printf("Pasar grados Fahrenheit\n");
     printf("Ingresa los grados Fahrenheit: ");
     scanf("%i", &fahr);
     float cels = (fahr - 32) * 5 / 9;
     printf("En grados Celsius: %f\n", cels);
+    return 0;
+}*/
+/*int main(){
+    int x = 10;
+    x += x * 2;
+    printf("x: %i\n", x);
+    return 0;
+}*/
+/*int main(){
+    int x;
+    printf("Ingresa un numero entero: ");
+    scanf("%i", &x);
+    x %= 5;
+    x = ++x;
+    printf("x: %i\n", x);
+}*/
+//-------------------------------------Practice of conditionals-------------------------------------------------
+/* int main(){
+    int x = 5;
+    int y;
+    printf("Adivina el numero del 1 al 10\n");
+    scanf("%i", &y);
+    if(x == y) {
+        printf("Has adivinado el numero!\n");
+    }
+    else{
+        printf("No has adivinado el numero\n");
+    }
+} */
+/* int main() {
+    int x, y;
+    printf("Ingresa el primer número: ");
+    scanf("%i", &x);
+    printf("Ingresa el segundo número: ");
+    scanf("%i", &y);
+    if(x < y) {
+        printf("Segun la condicion, el numero seleccionado es: %i\n", x);
+    }
+    else {
+        printf("Segun la condicion, el numero seleccionado es: %i\n", y);
+    }
+    return 0;
+} */
+int main() {
+    int x;
+    printf("Ingresa la nota del alumno, del 0 al 100: ");
+    scanf("%i", &x);
+    if(x < 60) {
+        printf("Suspendiste :( \n");
+    }
+    else if (x >= 90) {
+        printf("Enhorabuena, sacaste un sobresaliente \n");
+    }
+    else {
+        printf("Buen trabajo, has aprobado \n");
+    }
     return 0;
 }
